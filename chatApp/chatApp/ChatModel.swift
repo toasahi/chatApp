ec20c8fd0f30d1db59d2140e9ad8827a63e85aef
toasahi <asa1313to@gmail.com>
2021-11-13 00:10:25 +0900

    Add ChatModel

--- chatApp/chatApp/ChatModel.swift
+++ chatApp/chatApp/ChatModel.swift
@@ -5,3 +5,40 @@
 //
 
 import Foundation
+
+struct Chat:Identifiable{
+    var id:UUID{
+        person.id
+    }
+    let person: Person
+}
+
+struct Person:Identifiable{
+    let id = UUID()
+    let name: String
+    let imgString:String
+}
+
+struct Message:Identifiable{
+    
+    enum MessageType{
+        case Sent,Recived
+    }
+    
+    let id = UUID()
+    let date: Date
+    let text: String
+    let type: MessageType
+    
+    init(_ text:String,type:MessageType,date:Date){
+        self.date = date
+        self.text = text
+        self.type = type
+    }
+    
+    init(_ text:String,type:MessageType){
+        self.init(text,type:type,date:Date())
+    }
+    
+    
+}
